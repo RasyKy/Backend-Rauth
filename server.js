9b5b35ae3e060d0dfe429c2c6053a0abb7dcfa93
rauth123 <chhan.sophanyrauth24@kit.edu.kh>
2025-11-21 14:30:54 +0700

    Fix duplicate express declaration in server.js

--- server.js
+++ server.js
@@ -20,30 +20,6 @@ app.use('/api/mockexams', mockExamRoutes);
 app.use('/api/progress', progressRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 
-const express = require('express');
-const cors = require('cors');
-
-const lessonRoutes = require('./routes/lessonRoutes');
-const quizRoutes = require('./routes/quizRoutes');
-const mockExamRoutes = require('./routes/mockExamRoutes');
-
-
-// Any request starting with /api/lessons will be handled by lessonRoutes
-app.use('/api/lessons', lessonRoutes);
-
-// Any request starting with /api/quizzes will be handled by quizRoutes
-app.use('/api/quizzes', quizRoutes);
-
-// Any request starting with /api/mockexams will be handled by mockExamRoutes
-app.use('/api/mockexams', mockExamRoutes);
-
-
-// Start the server
-app.listen(port, () => {
-  console.log(`E-learning server running on http://localhost:${port}`);
-  console.log('Ready to handle requests for lessons, quizzes, and mock exams!');
-});
-
 app.listen(port, () => {
   console.log(`E-learning server running on http://localhost:${port}`);
   console.log('Ready to handle requests!');
